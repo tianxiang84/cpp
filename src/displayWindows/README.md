c57b326ceb30ea10e5caf9f09f143343f0ae7af0
tianxiang84 <tsu@slb.com>
2020-03-24 16:28:31 -0400

    To open a window

---
+++ src/displayWindows/README.md
@@ -0,0 +1,6 @@
+Problem:
+
+Cannot use mouse to copy.
+Ctrl+C does copy but it copies everything....
+
+To solve
---
+++ src/displayWindows/displayWindow.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+//#include <string>
+
+int main() {
+  MessageBox(0, "Hello World!", "Greetings", 0);
+
+  //System.Windows.Forms.Clipboard.SetDataObject(messageToShowInMsgBoxString, true);
+
+  //string msgtext = "message text";
+  //if (MessageBox.Show(msgtext, "bla bla bla. (OK to copy)", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+  //{ Clipboard.SetText(msgtext); }
+  
+  return 0;
+}
